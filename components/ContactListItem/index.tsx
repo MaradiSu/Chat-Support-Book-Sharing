009bab21648f3fdf3f948a1905eec1e3a1adeeee
MaradiSu <p.suresh@campusuvce.in>
2021-06-10 21:22:56 +0530

    initial commit

---
+++ components/ContactListItem/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+  Alert
+} from "react-native";
+import { User } from "../../types";
+import styles from "./style";
+import moment from "moment";
+import { useNavigation } from '@react-navigation/native';
+
+export type ContactListItemProps = {
+  user: User;
+}
+
+const ContactListItem = (props: ContactListItemProps) => {
+  const { user } = props;
+
+  const navigation = useNavigation();
+
+  const onClick = () => {
+   // navigate to chat room with this user
+   Alert.alert("request has been sent to "+user.name);
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={onClick}>
+      <View style={styles.container}>
+        <View style={styles.lefContainer}>
+          <Image source={{ uri: user.imageUri }} style={styles.avatar}/>
+
+          <View style={styles.midContainer}>
+            <Text style={styles.username} onPress={onClick} >{user.name}</Text>
+            <Text numberOfLines={2} style={styles.status}>{user.status}</Text>
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  )
+};
+
+export default ContactListItem;
---
+++ constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = 'green';
+const tintColorDark = 'grey';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
